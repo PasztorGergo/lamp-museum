83e938076fbc99bdbe63686e19083a5a8db3e1be
PasztorGergo <pasztorg05@gmail.com>
2022-07-12 18:16:13 +0200

    Final touches

--- lib/connect.ts
+++ lib/connect.ts
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export default async function connectDB() {
   mongoose.connect(
     //@ts-ignore
-    "mongodb+srv://PGergo:Nemtudom043@gergoapps.81peh.mongodb.net",
+    process.env.MOGNO_URI,
     { dbName: "Lampamuzeum" }
   );
 }
